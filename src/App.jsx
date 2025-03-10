45a6e7ec2a7ae88343b38c1c7c8d6c78f01a65b6
Ramon Noleto <devnoleto@gmail.com>
2025-03-10 20:25:36 -0300

    Criação do ProtectedRouter

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,10 @@ import Header from "./Components/Header";
 import Footer from "./Components/Footer";
 import Home from "./Components/Home";
 import Login from "./Components/Login/Login";
+import User from "./Components/User/User";
 
 import { UserStorage } from "./UserContext";
+import ProtectedRoute from "./Components/Helper/ProtectedRoute";
 
 const App = () => {
   return (
@@ -17,7 +19,15 @@ const App = () => {
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/login/*" element={<Login />} />
+            <Route path="login/*" element={<Login />} />
+            <Route
+              path="conta/*"
+              element={
+                <ProtectedRoute>
+                  <User />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
           <Footer />
         </UserStorage>
---
+++ src/Components/User/User.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const User = () => {
+  return (
+    <div>
+      Usuário aqui
+    </div>
+  )
+}
+
+export default User
